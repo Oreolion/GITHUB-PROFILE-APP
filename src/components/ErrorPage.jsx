9e21279749578ba3e15a81c112cd360592c6bb65
Your username <Your email address>
2023-10-22 06:11:28 -0700

    ADD: redirected to home

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
     <>
       <section style={{ textAlign: "center", marginTop: 200 }}>
         <p>Error 404!!!</p>
-        <Link to='/posts/about/${each.name}' style={{ fontSize: 20, color: "red"}} >
+        <Link to='/' style={{ fontSize: 20, color: "red"}} >
         &#60; &#60; &#60; return to previous page
         </Link>
       </section>
